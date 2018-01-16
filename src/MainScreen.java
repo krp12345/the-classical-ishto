7c3f4f8c9f3657a75f0ec61f02b5e95a6259d8c6
Rushikesh Kavar <kavarrushikesh1212@gmail.com>
2018-01-16 13:35:30 +0530

    Add files via upload

---
+++ src/MainScreen.java
@@ -0,0 +1,40 @@
+/*
+  welcome screen class 
+*/
+import javafx.application.Application;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class MainScreen  extends Application {
+
+    private int numberOfPlayers;
+    
+    public MainScreen() {
+        this.numberOfPlayers = 50;
+    }
+    
+    int getPlayerNumber(){
+        return this.numberOfPlayers;
+    }
+    
+    public static void main(String[] args) throws Exception {
+        Application.launch();
+        
+    }
+
+    //start method is abstract method that should be overrided.
+    //only argument is Stage object.
+    // In our application, Stage object is named primaryStage object 
+    //which is created by platform itself.
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Welcome Screen");
+        
+        primaryStage.setFullScreen(true);
+        
+        primaryStage.getIcons().add(new Image(MainScreen.class.getResourceAsStream("icon.png")));
+        //here icon is temporary . proper design of icon required
+        primaryStage.show();
+        //show method make stage visible
+    }
+
+}
