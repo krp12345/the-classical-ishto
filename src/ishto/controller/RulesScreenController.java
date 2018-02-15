35d4ecdf8fceb2290afedd7e33f8b3366bbcaeef
Rushikesh Kavar <kavarrushikesh1212@gmail.com>
2018-02-15 19:18:38 +0530

    Update RulesScreenController.java

--- src/ishto/controller/RulesScreenController.java
+++ src/ishto/controller/RulesScreenController.java
@@ -1,6 +1,9 @@
 package ishto.controller;
 import ishto.ApplicationStarter;
 import javafx.fxml.FXML;
+
+import static ishto.ApplicationStarter.primaryStage;
+
 public class RulesScreenController {
     public RulesScreenController() {
         System.out.println("i created");
@@ -8,5 +11,6 @@ public class RulesScreenController {
     @FXML
     private void onMainManuButtonClicked(){
         ApplicationStarter.primaryStage.getScene().setRoot(ApplicationStarter.mainScnVBox);
+        primaryStage.sizeToScene();
     }
 }
