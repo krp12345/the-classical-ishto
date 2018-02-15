e0168a145485536d021e980a805dff6364c2db4a
Rushikesh Kavar <kavarrushikesh1212@gmail.com>
2018-02-15 17:38:21 +0530

    Update NumberOfPlayersController.java

--- src/ishto/controller/NumberOfPlayersController.java
+++ src/ishto/controller/NumberOfPlayersController.java
@@ -1,40 +1,74 @@
 package ishto.controller;
 
 import ishto.ApplicationStarter;
+import ishto.logic.Game;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 
+import static ishto.ApplicationStarter.primaryStage;
+
 public class NumberOfPlayersController {
-    public Scene prefScene;
+    //public static Scene prefScene;
+    public static VBox prefScene;
+    public static int numberOfPlayers=-1;
     public NumberOfPlayersController() throws IOException{
-        System.out.println("here i am");
+        System.out.println("NumberOfPlayersController object constructed");
         FXMLLoader loaderPlayerPref;
-        loaderPlayerPref=new FXMLLoader();
-        File player_pref;
-        player_pref=new File("/home/rushikesh/IdeaProjects/the classical ishto/src/ishto/res/fxml/player_name_colour_scene.fxml");
+        loaderPlayerPref=new FXMLLoader(getClass().getResource("fxml/player_name_colour_scene.fxml"));
+        if(loaderPlayerPref ==null){
+            System.out.println("can not load FXMLoader ( player name ..fxml)");
+        }
 
-        FileInputStream streamPlayerPref;
-        streamPlayerPref=new FileInputStream(player_pref);
         VBox vBoxForPlayerPref;
-        vBoxForPlayerPref= loaderPlayerPref.load(streamPlayerPref);
-        prefScene=new Scene(vBoxForPlayerPref);
-
+        Object obj=loaderPlayerPref.load();
+        vBoxForPlayerPref= (VBox) obj;
+        //prefScene=new Scene(vBoxForPlayerPref);
+        prefScene=vBoxForPlayerPref;
+        System.out.println("NumberOfPlayersController object created");
     }
-
     @FXML
     private void onBackButtonAction()
     {
-        ApplicationStarter.prStage.setScene(ApplicationStarter.mainScn);
+
+        ApplicationStarter.primaryStage.getScene().setRoot(ApplicationStarter.mainScnVBox);
+        primaryStage.sizeToScene();
     }
 
     @FXML
     private void onContinueClicked(){
-        ApplicationStarter.prStage.setScene(prefScene);
+
+        if(numberOfPlayers==-1){
+            return;
+        }
+
+        System.out.println("number of players "+numberOfPlayers);
+
+
+        ApplicationStarter.primaryStage.getScene().setRoot(prefScene);
+        primaryStage.sizeToScene();
+
+    }
+
+    @FXML
+    private void onTwoPlayerSelected(){
+        numberOfPlayers=2;
+    }
+
+    @FXML
+    private void onThreePlayerSelected(){
+        numberOfPlayers=3;
+    }
+
+    @FXML
+    private void onFourPlayerSelected(){
+        numberOfPlayers=4;
+    }
+
+    @FXML
+    private void onMinimizeButtonClick(){
+        ApplicationStarter.primaryStage.setIconified(true);
     }
 }
